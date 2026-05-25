8522eb4905cfdad635f03dc4300935b639625e4c
Minhlvh114 <minhlvhhe163657@fpt.edu.vn>
2026-05-25 23:21:31 +0700

    init project

---
+++ project-team/electronic-shop/Server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const {connectDB} = require('./Model/index.js');
+const routers = require("./Route/index.js");
+// const logger = require("morgan");
+require('dotenv').config();
+
+app.use(express.json());
+// app.use(logger("dev"));
+
+connectDB();
+
+app.get('/', async(req, res)=>{
+    try {
+        res.send({message: 'Welcome to Back-end App! This will communicate other App with JSON'});
+    } catch (error) {
+        res.send({error: error.message});
+    }
+});
+
+app.get("/", (req, res) => res.send("Welcome to Court Management System"));
+
+app.use('/user/', routers.user);
+app.use('/category/', routers.category);
+app.use('/product/', routers.product);
+app.use('/product-detail/', routers.productDetail);
+app.use('/brand/', routers.brand);
+
+// app.use('/bill/', routes.crudA);
+
+
+
+const PORT = process.env.PORT 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
