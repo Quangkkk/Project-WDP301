b3091dade99918037a811929fbf335d7aa71458e
Dung Dinh <dinhdung2812@gmail.com>
2026-05-31 02:31:34 +0700

    refactor: config files

--- project-team/electronic-shop/Server/server.js
+++ project-team/electronic-shop/Server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const {connectDB} = require('./Model/index.js');
+const connectDB = require("./config/db");
 const routers = require("./Route/index.js");
 // const logger = require("morgan");
 require('dotenv').config();
