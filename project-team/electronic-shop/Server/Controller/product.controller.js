f6596c1404b097226c4c5c8e6947c5ce14886d67
Dung Dinh <dinhdung2812@gmail.com>
2026-06-06 11:20:18 +0700

    update: product controller

--- project-team/electronic-shop/Server/Controller/product.controller.js
+++ project-team/electronic-shop/Server/Controller/product.controller.js
@@ -1,7 +1,7 @@
 const Product = require("../Model/Products.model");
 const Variant = require("../Model/Variant.model");
-require("../Model/Brand.model");
-require("../Model/Categories.model");
+const Brand = require("../Model/Brand.model");
+const Category = require("../Model/Categories.model");
 
 const getAllProducts = async (req, res) => {
   try {
@@ -80,7 +80,102 @@ const getProductById = async (req, res) => {
   }
 };
 
+const getProductByCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category is not existed",
+      });
+    }
+
+    const product = await Product.find({ categoryId: id })
+      .populate("brandId", "name slug imageUrl")
+      .populate("categoryId", "name slug")
+      .lean();
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    const productIds = products.map((product) => product._id);
+
+    const variants = await Variant.find({
+      productId: { $in: productIds },
+      status: "active",
+    }).lean();
+
+    const productsWithVariants = products.map((product) => {
+      return {
+        ...product,
+        variants: variants.filter(
+          (variant) => variant.productId.toString() === product._id.toString()
+        ),
+      };
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Get products successfully",
+      count: productsWithVariants.length,
+      data: productsWithVariants,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to get products",
+      error: error.message,
+    });
+  }
+};
+
+const getProductByBrand = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id)
+      .populate("brandId", "name slug imageUrl")
+      .populate("categoryId", "name slug")
+      .lean();
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    const variants = await Variant.find({
+      productId: id,
+      status: "active",
+    }).lean();
+
+    return res.status(200).json({
+      success: true,
+      message: "Get product detail successfully",
+      data: {
+        ...product,
+        variants,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to get product detail",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   getAllProducts,
-  getProductById
+  getProductById,
+  getProductByCategory,
+  getProductByBrand
 };
--- project-team/electronic-shop/Server/Route/product.route.js
+++ project-team/electronic-shop/Server/Route/product.route.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = express.Router();
 const product = require('../Controller/product.controller')
 
-// router.post('/', product.addProduct);
 router.get('/', product.getAllProducts);
-// router.get('/category/:id', product.getProductByCategory);
-// router.get('/brand/:id', product.getProductByBrand);
+router.get('/category/:id', product.getProductByCategory);
+router.get('/brand/:id', product.getProductByBrand);
 router.get('/:id', product.getProductById);
-// router.put('/:id', product.updateProductById);
-// router.delete('/:id', product.deleteProductById);
 
 module.exports = router;
